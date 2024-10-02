0abba101d855b33b47a13115f2127b8fb586e267
Daniel Schulz <faulolio@gmail.com>
2024-10-02 17:21:52 +0200

    feat: server

--- server-deploy.bat
+++ server-deploy.bat
@@ -1,5 +1,9 @@
 setlocal
 FOR /F "tokens=*" %%i in ('type .env') do SET %%i
+findstr /C:"127.0.0.1 %STACK_NAME%" C:\Windows\System32\drivers\etc\hosts >nul 2>&1
+if %ERRORLEVEL% NEQ 0 (
+    echo 127.0.0.1 %STACK_NAME% >> C:\Windows\System32\drivers\etc\hosts
+)
 call composer-%PHP_VERSION% update
 call docker stack deploy %STACK_NAME% --detach=true --prune --resolve-image=always -c=docker-compose.yml
 call docker service update --force %STACK_NAME%_%STACK_NAME%
