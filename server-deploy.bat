21904470a33973da3d79267dbb3c9e8d8c454ae1
Daniel Schulz <faulolio@gmail.com>
2024-10-02 17:43:57 +0200

    build: update files

--- server-deploy.bat
+++ server-deploy.bat
@@ -4,7 +4,7 @@ findstr /C:"127.0.0.1 %STACK_NAME%" C:\Windows\System32\drivers\etc\hosts >nul 2
 if %ERRORLEVEL% NEQ 0 (
     echo 127.0.0.1 %STACK_NAME% >> C:\Windows\System32\drivers\etc\hosts
 )
-call composer-%PHP_VERSION% update
+call composer update
 call docker stack deploy %STACK_NAME% --detach=true --prune --resolve-image=always -c=docker-compose.yml
 call docker service update --force %STACK_NAME%_%STACK_NAME%
 endlocal
