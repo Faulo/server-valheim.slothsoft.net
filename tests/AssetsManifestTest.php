addf4414c5bfae0a2d0a607f461cb9e75a792395
Daniel Schulz <faulolio@gmail.com>
2026-05-13 14:36:02 +0200

    build: update files

--- tests/AssetsManifestTest.php
+++ tests/AssetsManifestTest.php
@@ -1,11 +1,12 @@
 <?php
 declare(strict_types = 1);
+
 namespace Slothsoft\Server\Valheim;
 
 use Slothsoft\FarahTesting\Module\AbstractXmlManifestTest;
 
 final class AssetsManifestTest extends AbstractXmlManifestTest {
-
+    
     protected static function getManifestDirectory(): string {
         return dirname(__DIR__) . DIRECTORY_SEPARATOR . 'assets';
     }
--- tests/AssetsModuleTest.php
+++ tests/AssetsModuleTest.php
@@ -1,13 +1,14 @@
 <?php
 declare(strict_types = 1);
+
 namespace Slothsoft\Server\Valheim;
 
 use Slothsoft\Farah\FarahUrl\FarahUrlAuthority;
 use Slothsoft\FarahTesting\Module\AbstractModuleTest;
 
 final class AssetsModuleTest extends AbstractModuleTest {
-
+    
     protected static function getManifestAuthority(): FarahUrlAuthority {
-        return FarahUrlAuthority::createFromVendorAndModule('slothsoft', 'valheim.slothsoft.net');
+        return FarahUrlAuthority::createFromVendorAndModule('slothsoft', 'archive.slothsoft.net');
     }
 }
